6a789aa51cc4b03e650240e3c4d0608d969eef84
Francesco Trotta <fratrotta@gmail.com>
2015-05-31 10:31:07 +0200

    Aggiunti script per aggiungere suffisso con nome utente alle directory dei repository. Aggiunti anche i file tutti.txt di 3ainf e 4ainf

---
+++ rename_repodir_usersuffix.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+base_dir=$1
+
+if [ -z "${base_dir}" ]
+then
+	echo 'ERROR: missing parameter' >/dev/stderr
+	echo "$0 <base_dir>" >/dev/stderr
+	exit 1
+fi
+
+user_dir_list=$(find ${base_dir} -type d -mindepth 1 -maxdepth 1)
+
+for user_dir in ${user_dir_list}
+do
+	user_name=$(basename ${user_dir})
+	subdir_list=$(find ${user_dir} -type d -mindepth 1 -maxdepth 1)
+	for subdir in ${subdir_list}
+	do
+		# Verifichiamo se la directory sia già stata rinominata
+		echo ${subdir} | grep -e ${user_name}$ &>/dev/null
+		if [ $? -ne 0 ]
+		then
+			mv "${subdir}" "${subdir}_${user_name}"
+			if [ $? -eq 0 ]
+			then
+				echo "${subdir} => ${subdir}_${user_name}}"
+			else
+				echo 'Exiting because of error in renaming' >/dev/stderr
+				exit 2
+			fi
+		fi
+	done
+done
